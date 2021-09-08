30e7e83344ba0ecf707be6d50405fec2d9438d03
tarik mamou <tarik339@gmail.com>
2021-09-09 00:12:16 +0200

    non blocking reactve client suite

---
+++ recative_web_client/src/main/java/partie7/reactive_web_client/ReactiveNonBockingClient.java
@@ -0,0 +1,9 @@
+package partie7.reactive_web_client;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+@Configuration
+public class ReactiveNonBockingClient {
+
+}
